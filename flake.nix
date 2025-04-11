8391c7fb9c5d21c8a95bbb899c550679d812e0a6
Callum Dunster <cdunster@users.noreply.github.com>
2025-04-11 12:45:30 +0200

    feat: add basic Colmena config for testing

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS configuration for a Nomad client, deployed using Colmena";
+
+  inputs = {
+    holonix.url = "github:holochain/holonix?ref=main-0.4";
+    nixpkgs.follows = "holonix/nixpkgs";
+  };
+
+  outputs = { ... }@inputs: {
+    colmena = {
+      meta = {
+        nixpkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+        };
+      };
+
+      nomad-client = { ... }: {
+        deployment = {
+          allowLocalDeployment = true;
+        };
+        boot.loader.grub.device = "/dev/sda";
+        fileSystems."/" = {
+          device = "/dev/sda1";
+          fsType = "ext4";
+        };
+      };
+    };
+  };
+}
