871ce9877bf7d74fec9a5baacc6caedccc582ef9
Callum Dunster <cdunster@users.noreply.github.com>
2025-04-14 11:43:45 +0200

    feat: add Wind Tunnel substituters for cached bins

--- flake.nix
+++ flake.nix
@@ -22,6 +22,17 @@
             allowLocalDeployment = true;
           };
 
+          nix = {
+            # Extra lines to be added to /etc/nix/nix.conf
+            extraOptions = "experimental-features = nix-command flakes";
+
+            # Add wind-tunnel substituters
+            settings = {
+              substituters = [ "https://cache.nixos.org" "https://holochain-ci.cachix.org" "https://holochain-wind-tunnel.cachix.org" ];
+              trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8=" "holochain-wind-tunnel.cachix.org-1:tnSm+7Y3hDKOc9xLdoVMuInMA2AQ0R/99Ucz5edYGJw=" ];
+            };
+          };
+
           system.stateVersion = "24.11";
 
           networking.hostName = name;
