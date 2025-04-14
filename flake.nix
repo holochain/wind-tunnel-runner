70b24bd3ecfd1bebc09a05703c836a89fe115d09
Callum Dunster <cdunster@users.noreply.github.com>
2025-04-14 12:09:51 +0200

    chore: add devShell with pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -4,23 +4,95 @@
   inputs = {
     holonix.url = "github:holochain/holonix?ref=main-0.4";
     nixpkgs.follows = "holonix/nixpkgs";
+    flake-parts.follows = "holonix/flake-parts";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-        config.allowUnfreePredicate = pkg: builtins.elem (inputs.nixpkgs.lib.getName pkg) [ "nomad" ];
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "aarch64-darwin" "x86_64-linux" ];
+
+    perSystem = { self', pkgs, system, ... }: {
+      formatter = pkgs.nixpkgs-fmt;
+
+      checks.pre-commit = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          # Nix
+          deadnix.enable = true;
+          nixpkgs-fmt.enable = true;
+          statix.enable = true;
+
+          # Spell checking
+          typos.enable = true;
+
+          # Git
+          check-merge-conflicts.enable = true;
+          no-commit-to-branch.enable = true;
+
+          # Whitespace
+          mixed-line-endings.enable = true;
+          trim-trailing-whitespace.enable = true;
+
+          # Markdown
+          markdownlint = {
+            enable = true;
+            settings.configuration = {
+              # Don't check line length in code blocks
+              line-length.code_blocks = false;
+            };
+          };
+          mdformat.enable = true;
+
+          # Private keys
+          detect-private-keys.enable = true;
+        };
       };
-    in
-    {
-      colmena = {
+
+
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          self'.checks.pre-commit.enabledPackages
+          colmena
+        ];
+
+        inherit (self'.checks.pre-commit) shellHook;
+      };
+
+      packages.default = pkgs.writeShellApplication {
+        name = "setup-script";
+        text = ''
+          if [[ -v 1 ]]; then
+              node="$1"
+          else
+              read -r -p "Enter node name: " node
+          fi
+
+          cd ${./.}
+          ${pkgs.colmena}/bin/colmena apply-local --sudo --impure --node="$node"
+          while ! ${pkgs.procps}/bin/pgrep "tailscaled" > /dev/null; do
+            sleep 0.5
+          done
+          ${pkgs.tailscale}/bin/tailscale up --ssh --hostname="$node"
+        '';
+      };
+    };
+
+    flake.colmena =
+      let
+        targetSystem = "x86_64-linux";
+      in
+      {
         meta = {
-          nixpkgs = pkgs;
+          nixpkgs = import inputs.nixpkgs {
+            system = targetSystem;
+            config.allowUnfreePredicate = pkg: builtins.elem (inputs.nixpkgs.lib.getName pkg) [ "nomad" ];
+          };
         };
 
-        defaults = { name, ... }: {
+        defaults = { name, pkgs, ... }: {
           deployment = {
             allowLocalDeployment = true;
           };
@@ -68,7 +140,7 @@
               bzip2
               telegraf
               # Enable unstable and non-default features that Wind Tunnel tests.
-              (inputs.holonix.packages.${system}.holochain.override { cargoExtraArgs = "--features chc,unstable-functions,unstable-countersigning"; })
+              (inputs.holonix.packages.${targetSystem}.holochain.override { cargoExtraArgs = "--features chc,unstable-functions,unstable-countersigning"; })
             ];
 
             # The Nomad configuration file
@@ -86,22 +158,5 @@
         };
       };
 
-      packages.${system}.default = pkgs.writeShellApplication {
-        name = "setup-script";
-        text = ''
-          if [[ -v 1 ]]; then
-              node="$1"
-          else
-              read -r -p "Enter node name: " node
-          fi
-
-          cd ${./.}
-          ${pkgs.colmena}/bin/colmena apply-local --sudo --impure --node="$node"
-          while ! ${pkgs.procps}/bin/pgrep "tailscaled" > /dev/null; do
-            sleep 0.5
-          done
-          ${pkgs.tailscale}/bin/tailscale up --ssh --hostname="$node"
-        '';
-      };
-    };
+  };
 }
