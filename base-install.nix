b6d0030236f1a866c28368f1c3fa7b8587ea3ed1
Callum Dunster <cdunster@users.noreply.github.com>
2025-05-01 10:28:04 +0200

    feat: add missing configuration to the base install

--- base-install.nix
+++ base-install.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, lib, inputs, ... }: {
   boot = {
     # Kernel modules available for use during the boot process. Must include all modules necessary for mounting the root device
     initrd.availableKernelModules = [
@@ -38,11 +38,17 @@
 
     # Enable the `nix` command and `flakes`
     extraOptions = "experimental-features = nix-command flakes";
+
+    # Add wind-tunnel substituters
+    settings = {
+      substituters = [ "https://cache.nixos.org" "https://holochain-ci.cachix.org" "https://holochain-wind-tunnel.cachix.org" ];
+      trusted-public-keys = [ "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" "holochain-ci.cachix.org-1:5IUSkZc0aoRS53rfkvH9Kid40NpyjwCMCzwRTXy+QN8=" "holochain-wind-tunnel.cachix.org-1:tnSm+7Y3hDKOc9xLdoVMuInMA2AQ0R/99Ucz5edYGJw=" ];
+    };
   };
 
   networking = {
-    # Set the machine's name
-    hostName = "nomad-client";
+    # Set the default machine's name
+    hostName = lib.mkDefault "nomad-client";
 
     # Enable DHCP for all network devices
     useDHCP = true;
@@ -52,8 +58,40 @@
     # Disable using `passwd` to change user passwords
     mutableUsers = false;
 
-    # Set the password of the root user
-    extraUsers.root.password = "init";
+    # Set the password of the root user to the one in the password manager
+    extraUsers.root.hashedPassword = lib.mkDefault "$y$j9T$LEwPZpyLzb3CKDBEtAi.w1$Uxok0mk4i5AWJ0zbPaqfY6T7Bw5nNYteu69yxqD7Mg/";
+  };
+
+  # Enable Tailscale, used for SSH access
+  services.tailscale.enable = true;
+
+  # Enable Nomad as a client node
+  services.nomad = {
+    enable = true;
+    dropPrivileges = false; # Clients require root privileges
+
+    extraPackages = with pkgs; [
+      coreutils
+      bash
+      hexdump
+      gnutar
+      bzip2
+      telegraf
+      # Enable unstable and non-default features that Wind Tunnel tests.
+      (inputs.holonix.packages.x86_64-linux.holochain.override { cargoExtraArgs = "--features chc,unstable-functions,unstable-countersigning"; })
+    ];
+
+    # The Nomad configuration file
+    settings = {
+      data_dir = "/var/lib/nomad";
+      plugin.raw_exec.config.enabled = true;
+      acl.enabled = true;
+      client = {
+        enabled = true;
+        servers = [ "nomad-server-01.holochain.org" ];
+        artifact.disable_filesystem_isolation = true;
+      };
+    };
   };
 
   # This value determines the NixOS release from which the default
