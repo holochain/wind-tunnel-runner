95c8f5000ae1eb847bb300e81d7eeaa0f873e8d7
Jost <28270981+jost-s@users.noreply.github.com>
2025-05-09 06:59:02 -0600

    build: add jost's test node

--- colmena.nix
+++ colmena.nix
@@ -66,4 +66,11 @@ in
   };
 
   nomad-client-cdunster = _: { };
+
+  jost-test-os-terone = _: {
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/7b99b518-aaac-4204-9c3c-131f26d11e0a";
+      fsType = "ext4";
+    };
+  };
 }
