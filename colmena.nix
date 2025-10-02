b13d46c74ce7c349e0a14e418f7991dbc5b069d9
Callum Dunster <cdunster@users.noreply.github.com>
2025-10-02 17:38:07 +0200

    feat: remove the now-unused `isUEFI` option

--- colmena.nix
+++ colmena.nix
@@ -22,8 +22,6 @@ in
   };
 
   nomad-client-1 = _: {
-    isUEFI = true;
-
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/a92690a8-d96c-4305-bfd9-ac4cf7f1c9e6";
       fsType = "ext4";
@@ -31,8 +29,6 @@ in
   };
 
   thetasinner-testoport = { config, ... }: {
-    isUEFI = true;
-
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/727efd61-af0f-4b5d-ab90-8b6fb3221c5b";
       fsType = "ext4";
@@ -48,36 +44,21 @@ in
   };
 
   nomad-client-zippy-hp-1 = _: {
-    isUEFI = false;
-
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/8cd1f3a9-c743-42de-833a-6b9769ebd758";
       fsType = "ext4";
     };
   };
 
-  nomad-client-cdunster = _: {
-    isUEFI = true;
-  };
+  nomad-client-cdunster = _: { };
 
-  jost-test-os-terone = _: {
-    isUEFI = true;
-  };
+  jost-test-os-terone = _: { };
 
-  nomad-client-zippy-hp-2 = _: {
-    isUEFI = false;
-  };
+  nomad-client-zippy-hp-2 = _: { };
 
-  nomad-client-zippy-hp-3 = _: {
-    isUEFI = false;
-  };
+  nomad-client-zippy-hp-3 = _: { };
 
-  nomad-client-zippy-hp-4 = _: {
-    isUEFI = false;
-  };
+  nomad-client-zippy-hp-4 = _: { };
 
-  nomad-client-zippy-hp-5 = _: {
-    isUEFI = false;
-  };
-
+  nomad-client-zippy-hp-5 = _: { };
 }
