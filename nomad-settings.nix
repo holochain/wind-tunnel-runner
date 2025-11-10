15bb7dbbc29b04ea4fdf3476467134a4191e2a8b
Callum Dunster <cdunster@users.noreply.github.com>
2025-11-10 15:48:27 +0100

    refactor: move Nomad settings into separate module
    This is so that the same settings can be used for the NixOS config as
    well as the future docker image.

---
+++ nomad-settings.nix
@@ -0,0 +1,10 @@
+{
+  data_dir = "/var/lib/nomad";
+  plugin.raw_exec.config.enabled = true;
+  acl.enabled = true;
+  client = {
+    enabled = true;
+    servers = [ "nomad-server-01.holochain.org" ];
+    artifact.disable_filesystem_isolation = true;
+  };
+}
