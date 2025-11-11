fbd562717786c3aad1a60ae3860bbe553f443997
Callum Dunster <cdunster@users.noreply.github.com>
2025-11-11 17:40:20 +0100

    refactor: add helper attrsets for the old system configs

--- colmena.nix
+++ colmena.nix
@@ -1,6 +1,30 @@
 inputs:
 let
   targetSystem = "x86_64-linux";
+
+  oldGrubOnlySystem = {
+    boot.loader.grub = {
+      device = "/dev/sda";
+      efiSupport = false;
+      efiInstallAsRemovable = false;
+    };
+
+    fileSystems."/efi-boot".enable = false;
+  };
+
+  oldSystemdBootSystem = {
+    boot.loader = {
+      grub = {
+        enable = false;
+        efiInstallAsRemovable = false;
+      };
+      systemd-boot.enable = true;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+  };
 in
 {
   meta = {
@@ -21,38 +45,14 @@ in
     networking.hostName = name;
   };
 
-  nomad-client-1 = _: {
-    boot.loader = {
-      grub = {
-        enable = false;
-        efiInstallAsRemovable = false;
-      };
-      systemd-boot.enable = true;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
-    };
-
+  nomad-client-1 = _: oldSystemdBootSystem // {
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/a92690a8-d96c-4305-bfd9-ac4cf7f1c9e6";
       fsType = "ext4";
     };
   };
 
-  thetasinner-testoport = { config, ... }: {
-    boot.loader = {
-      grub = {
-        enable = false;
-        efiInstallAsRemovable = false;
-      };
-      systemd-boot.enable = true;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
-    };
-
+  thetasinner-testoport = { config, ... }: oldSystemdBootSystem // {
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/727efd61-af0f-4b5d-ab90-8b6fb3221c5b";
       fsType = "ext4";
@@ -73,15 +73,7 @@ in
 
   nomad-client-thetasinner-hp-testoport-3 = _: { };
 
-  nomad-client-zippy-hp-1 = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-
+  nomad-client-zippy-hp-1 = _: oldGrubOnlySystem // {
     fileSystems."/" = {
       device = "/dev/disk/by-uuid/8cd1f3a9-c743-42de-833a-6b9769ebd758";
       fsType = "ext4";
@@ -90,72 +82,15 @@ in
 
   nomad-client-cdunster = _: { };
 
-  jost-test-os-terone = _: {
-    boot.loader = {
-      grub = {
-        enable = false;
-        efiInstallAsRemovable = false;
-      };
-      systemd-boot.enable = true;
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
-    };
-  };
+  jost-test-os-terone = _: oldSystemdBootSystem;
 
-  nomad-client-zippy-hp-2 = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-  };
-
-  nomad-client-zippy-hp-3 = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-  };
-
-  nomad-client-zippy-hp-4 = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-  };
-
-  nomad-client-zippy-hp-5 = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-  };
-
+  nomad-client-zippy-hp-2 = _: oldGrubOnlySystem;
+  nomad-client-zippy-hp-3 = _: oldGrubOnlySystem;
+  nomad-client-zippy-hp-4 = _: oldGrubOnlySystem;
+  nomad-client-zippy-hp-5 = _: oldGrubOnlySystem;
   nomad-client-zippy-hp-6 = _: { };
   nomad-client-zippy-hp-7 = _: { };
-
-  nomad-client-zippy-hp-8-n = _: {
-    boot.loader.grub = {
-      device = "/dev/sda";
-      efiSupport = false;
-      efiInstallAsRemovable = false;
-    };
-
-    fileSystems."/efi-boot".enable = false;
-  };
+  nomad-client-zippy-hp-8-n = _: oldGrubOnlySystem;
 
   sawin-hp-plus = _: { };
 }
